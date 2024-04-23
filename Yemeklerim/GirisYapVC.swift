9d86714ba2f4f306a9547e2dd71a9f467db6818c
Fırat ilhan <firatilhan008@gmail.com>
2024-04-24 00:36:53 +0300

    giris yap vc değişiklikler yapıldı

--- Yemeklerim/GirisYapVC.swift
+++ Yemeklerim/GirisYapVC.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GirisYapVC: UIViewController {
 
+    @IBOutlet weak var userPasswordLabel: UITextField!
+    @IBOutlet weak var userNameLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func girisYapButton(_ sender: Any) {
+    }
 }
 
