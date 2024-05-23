f6236663d9a4e47764681c2ade8d59644e7d8ac6
Fırat ilhan <firatilhan008@gmail.com>
2024-05-23 11:45:36 +0300

    beğenilen yemekler sayfası ve detay sayfasına aktarılması işlemi yapıldı

--- Yemeklerim/File.swift
+++ Yemeklerim/File.swift
@@ -1,9 +1,3 @@
-//func veriKontrol() {
-//    if yemekListesi.count == 0 {
-//        emptyMessageLabel.isHidden = false
-//        print("liste boş: \(yemekListesi.count)")
-//    } else {
-//        emptyMessageLabel.isHidden = true
-//        print("liste boş değil: \(yemekListesi.count)")
-//    }
-//}
+
+
+
